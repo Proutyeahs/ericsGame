0c9cdba4b6052f417be3175986a9d03e1db666b4
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-06-08 21:48:30 -0500

    tried adding species with javascript

--- content.js
+++ content.js
@@ -15,3 +15,23 @@ if (hourNow > 18) {
 }
     //the internet says doc.write is bad...?
 document.write('<h3>' + greeting + '</h3>');
+
+/* wow this is going to need alot of work.
+functions to keep track of all the stats and increase them as they level up.
+math floor(math random) etc.??? 1-3 stats gained per level, plus the starting stats?
+well looks like this is as far as I make it on this atm.
+*/
+
+function Species(name, description, type, lvl, hp, def, att, res) {
+    this.name = name;
+    this.description = description;
+    this.type = type;
+    this.lvl = lvl;
+    this.hp = hp;
+    this.def = def;
+    this.att = att;
+    this.res = res;
+}
+//Simple example for now, used the JS&JQuery book to learn this
+var glyptodonSpecies = new Species(Glyptodon, "Large heavily armored mammal", earth, 1, 10, 10, 10, 10);
+var teratornithidaeSpecies = new Species(Teratornithidae, "Large bird of pray", air, 1, 10, 10, 10, 10);
